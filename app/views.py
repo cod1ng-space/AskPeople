8dc44b7a9094218666d7fea9d47259eaf35a5d56
Daniil Voronin <danilka.voronin.05@gmail.com>
2025-04-07 12:08:35 +0300

    fix

--- app/views.py
+++ app/views.py
@@ -10,23 +10,28 @@ QUESTIONS = [
         'title': f'Title {i}',
         'id': i,
         'text': f'This is text for question #{i}'
-    } for i in range(50)
+    } for i in range(100)
 ]
 
 ANSWERS = [
     {
         'id': i,
         'text': f'This is text for answer #{i}'
-    } for i in range(50)
+    } for i in range(100)
 ]
 
-def get_page(request, array):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(array, 5)
+def paginate(object_list, request, per_page=10):
+    try:
+        page_num = int(request.GET.get('page', 1))
+    except ValueError: # Если не не число
+        page_num = 1 
+    
+    paginator = Paginator(object_list, per_page)
     if page_num < 1:
         page_num = 1
     elif page_num > paginator.num_pages:
         page_num = paginator.num_pages
+        
     try:
         page = paginator.page(page_num)
     except (EmptyPage, PageNotAnInteger): # Если paginator.num_pages == 0
@@ -35,21 +40,21 @@ def get_page(request, array):
 
 # Create your views here.
 def index(request):
-    page = get_page(request, QUESTIONS)
+    page = paginate(QUESTIONS, request)
 
     return render(request, 'index.html', context={'questions': page.object_list, 'page_obj': page})
 
 def hot(request):
     q = list(reversed(copy.deepcopy(QUESTIONS)))
-    page = get_page(request, q)
+    page = paginate(q, request)
     return render(request, 'hot.html', context={'questions': page.object_list, 'page_obj': page})
 
 def question(request, question_id):
-    page = get_page(request, ANSWERS)
+    page = paginate(ANSWERS, request)
     return render(request, 'single_question.html', context={'question': QUESTIONS[question_id], 'answers': page.object_list, 'page_obj': page})
 
 def tag(request, tag_name):
-    page = get_page(request, QUESTIONS)
+    page = paginate(QUESTIONS, request)
     return render(request, 'tag.html', context={'questions': page.object_list, 'page_obj': page, 'tag_name': tag_name})
 
 def login(request):
