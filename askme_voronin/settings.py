3937d341cd3b1835f245851b44b7fd314181a453
Daniil Voronin <danilka.voronin.05@gmail.com>
2025-04-08 20:04:15 +0300

    commit

--- askme_voronin/settings.py
+++ askme_voronin/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'askme_voronin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_yourdbname',
+        'USER': 'youruser',
+        'PASSWORD': 'yourpass',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
